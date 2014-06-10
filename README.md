506f402101f43101dcb7a1294f548822861eb547
mperdeck <mperdeck@gmail.com>
2014-06-10 20:58:25 +1000

    initial push

---
+++ README.md
@@ -0,0 +1,37 @@
+# Logging and JavaScript error handling for AngularJS
+
+## logToServer.js
+
+* Replaces standard AngularJS [$log](https://docs.angularjs.org/api/ng/service/$log) to version that logs to the server, and optionally to the console using a [consoleAppender](http://jsnlog.com/Documentation/WebConfig/JSNLog/ConsoleAppender).
+* Replaces AngularJS [$exceptionHandler](https://docs.angularjs.org/api/ng/service/$exceptionHandler) with version that logs JavaScript exceptions to the server.
+* Provides [Interceptors](https://docs.angularjs.org/api/ng/service/$http#interceptors) to log AJAX errors and timeouts to the server, and to log warning messages when AJAX responses are slow.
+
+## How to use
+
+[Install the JSNLog JavaScript Logging Package](http://jsnlog.com/). Copy logToServer.js to your site and load it into your pages, such as with a script tag.
+
+To use the new $log and $exceptionHandler, simply import module _logToServer_, like so:
+```
+var app = angular.module('mainmodule', ['logToServer']);
+```
+
+To use the interceptors, add them to the interceptors array for your application:
+```
+app.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.interceptors.push('logToServerInterceptor');
+}]);
+```
+
+Set the timeout and the delay after which a warning log message is sent in milliseconds in the config object, when you call $http:
+```
+$http({ method: '...', url: '...', data: ..., _timeout_: 5000, _warningAfter_: 2000 })
+.success(function (data, status, headers, config) {
+	...
+})
+.error(function (data, status, headers, config) {
+	...
+});
+```
+
+[More about AngularJS Error Handling](http://jsnlog.com/Documentation/GetStartedLogging/AngularJsErrorHandling)
+
---
+++ logToServer.js
@@ -0,0 +1,60 @@
+angular.module('logToServer', [])
+.service('$log', function () {
+    this.log = function (msg) {
+        JL('Angular').trace(msg);
+    }
+
+    this.debug = function (msg) {
+        JL('Angular').debug(msg);
+    }
+
+    this.info = function (msg) {
+        JL('Angular').info(msg);
+    }
+
+    this.warn = function (msg) {
+        JL('Angular').warn(msg);
+    }
+
+    this.error = function (msg) {
+        JL('Angular').error(msg);
+    }
+})
+.factory('$exceptionHandler', function () {
+    return function (exception, cause) {
+        JL('Angular').fatalException(cause, exception);
+        throw exception;
+    };
+})
+.factory('logToServerInterceptor', ['$q', function ($q) {
+
+    var myInterceptor = {
+        'request': function (config) {
+            config.msBeforeAjaxCall = new Date().getTime();
+            return config;
+        },
+        'response': function (response) {
+            if (response.config.warningAfter) {
+                var msAfterAjaxCall = new Date().getTime();
+                var timeTakenInMs = msAfterAjaxCall - response.config.msBeforeAjaxCall;
+                if (timeTakenInMs > response.config.warningAfter) {
+                    JL('Angular.Ajax').warn({ "timeTakenInMs": timeTakenInMs, config: response.config, data: response.data });
+                }
+            }
+
+            return response;
+        },
+        'responseError': function (rejection) {
+            var errorMessage = "timeout";
+            if (rejection.status != 0) {
+                errorMessage = rejection.data.ExceptionMessage;
+            }
+
+            JL('Angular.Ajax').fatalException({ errorMessage: errorMessage, status: rejection.status, config: rejection.config }, rejection.data);
+            return $q.reject(rejection);
+        }
+    };
+
+    return myInterceptor;
+}]);
+
