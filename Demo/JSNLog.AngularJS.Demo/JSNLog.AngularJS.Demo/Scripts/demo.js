ede4c11755b08c37d1ea440f61daa390c2b83fda
mperdeck <mperdeck@gmail.com>
2015-09-26 11:33:20 +1000

    version without logging now working

--- Demo/JSNLog.AngularJS.Demo/JSNLog.AngularJS.Demo/Scripts/demo.js
+++ Demo/JSNLog.AngularJS.Demo/JSNLog.AngularJS.Demo/Scripts/demo.js
@@ -1,7 +1,16 @@
 ﻿angular.module('demoModule', [])
-  .controller('DemoController', DemoController);
+  .controller('DemoController', ['$http', DemoController]);
 
-function DemoController() {
+function DemoController($http) {
+    this._$http = $http;
+}
+
+var showSuccessMessage = function (message) {
+    $('#message').text(message).css("color", 'green');
+}
+
+var showErrorMessage = function (message) {
+    $('#message').text(message).css("color", 'red');
 }
 
 DemoController.prototype.throw = function () {
@@ -9,6 +18,18 @@ DemoController.prototype.throw = function () {
 };
 
 DemoController.prototype.sendWait = function (ms) {
-    alert(ms);
+    $('#message').text('Please wait');
+    $('#spinner').show();
+
+    this._$http.get('/api/wait/' + ms)
+      .then(function(response) {
+          showSuccessMessage(response.data);
+      })
+      .catch(function(errorReason) {
+          showErrorMessage(errorReason.data.ExceptionMessage);
+      })
+      .finally(function() {
+          $('#spinner').hide();
+      });
 };
 
