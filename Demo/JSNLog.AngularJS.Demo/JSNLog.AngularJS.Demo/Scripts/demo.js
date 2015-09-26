60bcefbb0be4fc97b1d80d2c7e9c5a7b078f6681
mperdeck <mperdeck@gmail.com>
2015-09-26 10:15:01 +1000

    added angular

---
+++ Demo/JSNLog.AngularJS.Demo/JSNLog.AngularJS.Demo/Scripts/demo.js
@@ -0,0 +1,14 @@
+﻿angular.module('demoModule', [])
+  .controller('DemoController', DemoController);
+
+function DemoController() {
+}
+
+DemoController.prototype.throw = function () {
+    throw 0;
+};
+
+DemoController.prototype.sendWait = function (ms) {
+    alert(ms);
+};
+
